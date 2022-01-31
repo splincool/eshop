20ba6fca41e9eb6015fd5fd3b129476bcfab9b96
Maksim Ivanov <mak.ivanov@gmail.com>
2022-01-31 15:16:26 +0300

    update useCurrency

--- src/composables/useCurrency.ts
+++ src/composables/useCurrency.ts
@@ -1,11 +1,6 @@
-import { computed, watch, ComputedRef, unref } from 'vue';
+import { computed, ComputedRef, unref } from 'vue';
 
 export function useCurrency(price: string | number | ComputedRef<number>) {
-	watch(() => price, (newP) => {
-			console.log(newP);
-		}
-	);
-
 	const currency = computed(() => {
 		return Number(unref(price)).toLocaleString('en-US', {
 			style: 'currency',
